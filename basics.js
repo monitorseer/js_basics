4bb564e5ad7108b68ab1a83ad7c6453c8a2858c7
monitorseer <158588461+monitorseer@users.noreply.github.com>
2024-07-09 17:33:19 +0800

    Added dict and functions

--- basics.js
+++ basics.js
@@ -94,3 +94,25 @@ console.log(schools);
 for (let i = 0; i < schools.length; i++) {
     console.log(schools[i]);
 }
+
+// Objects (like dictionaries in Python)
+let person = {
+    firstName: "Maadhesh",
+    lastName: "Vetrimani",
+    age: 17,
+    isStudent: true,
+};
+
+person["age"] = 18;
+person["isStudent"] = false;
+console.log(person.age);
+console.log(person["firstName"], person["lastName"]);
+
+// Functions
+function cube(x) {
+    return x ** 3;
+}
+
+// Arrow functions (shorter)
+let magnitude = (x, y, z) => Math.sqrt(x**2 + y**2 + z**2);
+console.log(magnitude(3, 4, 5));
